8d65de324b4daae505a21094277ed3fe41bfc378
Rodrigo <rjaencobos@safareyes.es>
2024-10-03 17:34:00 +0200

    Add exercises 4, 5, 6, and 7 to index.html and implement functionality for exercise 3 in ut2.js

--- UT2/ut2.js
+++ UT2/ut2.js
@@ -55,5 +55,28 @@ function ejercicio2() {
 }
 
 function ejercicio3(){
-    
+    let alumnos = [
+        { nombre:"Juan", nota:"7.8" },
+        { nombre:"Maria", nota:"4.29" },
+        { nombre:"Alberto", nota:"10" },
+        { nombre:"Rosa", nota:"0.54" },
+    ];
+    let container = document.getElementById('alumnos-div');
+    alumnos.forEach(alumno => {
+        let alumnoDiv = document.createElement('div');
+        alumnoDiv.textContent = `Nombre: ${alumno.nombre}, Nota: ${alumno.nota}`;
+        container.appendChild(alumnoDiv);
+    });
+    let nombreAlumno = prompt("Introduce el nombre de un alumno:");
+    let alumno = alumnos.find(s => s.nombre.toLowerCase() === nombreAlumno.toLowerCase());
+    if (alumno){
+        alert("La nota de ${alumno.nombre} es ${alumno.nota}");
+    } else {
+        var nota = parseFloat(prompt("Una nota para el alumno:"));
+        while (nota< 0 || nota > 10){
+            var nota = parseFloat(prompt("Una nota para el alumno:"));
+        }
+        alumnos.push({nombre: nombreAlumno, nota: nota})
+        alert("Alumno ${nombreAlumno} añadido con la nota ${nota}");
+    }
 }
